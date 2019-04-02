827663607d948dc7b1979298b66e5899382dee71
Carl <Carl@android-f834eb0a60b36de1.localdomain>
2019-04-02 12:01:36 +0100

    Add your favourite movie cover. #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import crashposter from './crash-poster.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>Doing well on day 1</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Welcome to Kodflix!</h1>
+        <img src={crashposter} alt='Crash Poster' />
       </div>
     );
   }
