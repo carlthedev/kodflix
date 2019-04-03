878e42ee64d5023885a91f1c820a6e5a3a2188d0
Carl <Carl@android-f834eb0a60b36de1.localdomain>
2019-04-03 12:00:40 +0100

    Add tv show covers. #3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-import crashposter from './crash-poster.jpg';
+import breakingbad from './images/bb-cover.jpg'
+import gameofthrones from './images/got-cover.jpg'
+import bettercallsaul from './images/bcs-cover.jpg'
+import walkingdead from './images/wd-cover.jpg'
+import prisonbreak from './images/pd-cover.jpg'
+import gotham from './images/g-poster.jpg'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome to Kodflix!</h1>
-        <img src={crashposter} alt='Crash Poster' />
+        <br /><br /><br />
+        <div className='container'>
+          <div className='item'>
+            <img src={breakingbad} alt='breaking bad cover' />
+          </div>
+          <div className='item'>
+            <img src={gameofthrones} alt='game of thrones cover' />
+          </div>
+          <div className='item'>
+            <img src={bettercallsaul} alt='better call saul cover' />
+          </div>
+        </div>
+        <div className='container'>
+          <div className='item'>
+            <img src={walkingdead} alt='walking dead cover' />
+          </div>
+          <div className='item'>
+            <img src={prisonbreak} alt='prison break cover' />
+          </div>
+          <div className='item'>
+            <img src={gotham} alt='gotham cover' />
+          </div>
+        </div>
       </div>
     );
   }
