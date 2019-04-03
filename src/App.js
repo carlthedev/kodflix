f0fd2ca70ba82f275dec643847c9ca6d218aa331
Carl <Carl@android-f834eb0a60b36de1.localdomain>
2019-04-03 13:23:48 +0100

    Add the TV show title overlay on hover-over #5

--- src/App.js
+++ src/App.js
@@ -16,23 +16,41 @@ class App extends Component {
         <div className='container'>
           <div className='item'>
             <img src={breakingbad} alt='breaking bad cover' />
+            <div className='overlay'>
+              <h1>Breaking Bad</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={gameofthrones} alt='game of thrones cover' />
+            <div className='overlay'>
+              <h1>Game Of Thrones</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={bettercallsaul} alt='better call saul cover' />
+            <div className='overlay'>
+              <h1>Better Call Saul</h1>
+            </div>
           </div>
         </div>
         <div className='container'>
           <div className='item'>
             <img src={walkingdead} alt='walking dead cover' />
+            <div className='overlay'>
+              <h1>Walking Dead</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={prisonbreak} alt='prison break cover' />
+            <div className='overlay'>
+              <h1>Prison Break</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={gotham} alt='gotham cover' />
+            <div className='overlay'>
+              <h1>Gotham</h1>
+            </div>
           </div>
         </div>
       </div>
