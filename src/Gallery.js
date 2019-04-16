b5e97f3a1a5c45d3b47046288ae61ab953afdc3f
Carl <Carl@android-f834eb0a60b36de1.localdomain>
2019-04-16 13:03:06 +0100

    Move the covers data model to a proper front-end data source  # 12

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import Tvshow from './Tvshow';
-import breakingbad from './images/bb-cover.jpg';
-import gameofthrones from './images/got-cover.jpg';
-import bettercallsaul from './images/bcs-cover.jpg';
-import walkingdead from './images/wd-cover.jpg';
-import prisonbreak from './images/pd-cover.jpg';
-import gotham from './images/g-poster.jpg';
+import TvShow from './Tvshow';
+import getGallery from './gallery-get';
 
 export default function Gallery() {
-    return(
-      <div>
-        <div className='container'>
-            <Tvshow id='breakingbad' name='Breaking bad' cover={breakingbad} />
-            <Tvshow id='gameofthrones' name='Game Of Thrones' cover={gameofthrones} />
-            <Tvshow id='bettercallsaul' name='Better call saul' cover={bettercallsaul} />
-            <Tvshow id='walkingdead' name='Walking Dead' cover={walkingdead} />
-            <Tvshow id='prisonbreak' name='Prison Break' cover={prisonbreak} />
-            <Tvshow id='gotham' name='Gotham' cover={gotham} />
-          </div>
+  return (
+    <div>
+      <div className='container'>
+        {
+          getGallery().map(Tvshow => (
+            <TvShow
+              key={Tvshow.id}
+              id={Tvshow.id}
+              name={Tvshow.name}
+              cover={Tvshow.cover} />
+          ))
+        }
       </div>
-      
-    )
-  }
+    </div>
+  );
+}
+
---
+++ src/gallery-get.js
@@ -0,0 +1,17 @@
+import breakingbad from './images/bb-cover.jpg';
+import gameofthrones from './images/got-cover.jpg';
+import bettercallsaul from './images/bcs-cover.jpg';
+import walkingdead from './images/wd-cover.jpg';
+import prisonbreak from './images/pd-cover.jpg';
+import gotham from './images/g-poster.jpg';
+
+export default function getGallery() {
+    return [
+      { id: 'breakingbad', name: 'Breaking bad', cover: breakingbad },
+      { id: 'gameofthrones', name: 'Game Of Thrones', cover: gameofthrones },
+      { id: 'bettercallsaul', name: 'Better call saul', cover: bettercallsaul },
+      { id: 'walkingdead', name: 'Walking Dead', cover: walkingdead },
+      { id: 'prisonbreak', name: 'Prison Break', cover: prisonbreak },
+      { id: 'gotham', name: 'Gotham', cover: gotham }
+    ];
+  }
