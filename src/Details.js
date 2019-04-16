ab1c8a6fe162d9593cc67949325fbbd9d46574b3
Carl <Carl@android-f834eb0a60b36de1.localdomain>
2019-04-16 13:55:32 +0100

    Introduce component state #13

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Details() {
+export default class Details extends React.Component {
+
+  constructor() {
+    super();
+    debugger;
+    this.state = {
+        welcomeMessage: 'Welcome to the Gallery page, WIP!!.'
+    };
+  }
+
+  componentDidMount() {
+    debugger;
+    setTimeout(() => {
+      this.setState({
+        welcomeMessage: 'The best is yet to come!'
+      });
+    }, 3000);
+  }
+
+  render() {
+    debugger;
     return (
-      <div><h1>Welcome to the Gallery page, WIP.</h1>
+      <div><h1>{this.state.welcomeMessage}</h1>
       <Link to='/'>Back to homepage</Link>
       </div>
-    )
+    );
   }
+}
